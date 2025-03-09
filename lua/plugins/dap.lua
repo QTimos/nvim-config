f955416903e1797dccd04f9abdfeab731c254906
QTimos <timos.wapo@protonmail.com>
2025-03-09 21:04:23 +0000

    dap.ui modified

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,10 +3,24 @@ return {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 	},
-	config = function ()
+	config = function()
 		local dap = require("dap")
+		local dapui = require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
 		vim.keymap.set("n", "<leader>tb", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<leader>c", dap.continue, {})
-	end
+	end,
 }
