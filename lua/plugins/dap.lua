6e2086aa4cfd5d71f3b62d3f8f6b400992f096f1
QTimos <timos.wapo@protonmail.com>
2025-03-09 21:37:33 +0000

    dap.lua modified

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,6 +8,96 @@ return {
 		local dap = require("dap")
 		local dapui = require("dapui")
 
+		-- gdb for C, C++ and Rust
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "Select and attach to process",
+				type = "gdb",
+				request = "attach",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				pid = function()
+					local name = vim.fn.input("Executable name (filter): ")
+					return require("dap.utils").pick_process({ filter = name })
+				end,
+				cwd = "${workspaceFolder}",
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "gdb",
+				request = "attach",
+				target = "localhost:1234",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			},
+		}
+
+		-- debugpy for Python
+		dap.adapters.python = function(cb, config)
+			if config.request == "attach" then
+				---@diagnostic disable-next-line: undefined-field
+				local port = (config.connect or config).port
+				---@diagnostic disable-next-line: undefined-field
+				local host = (config.connect or config).host or "127.0.0.1"
+				cb({
+					type = "server",
+					port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+					host = host,
+					options = {
+						source_filetype = "python",
+					},
+				})
+			else
+				cb({
+					type = "executable",
+					command = "path/to/virtualenvs/debugpy/bin/python",
+					args = { "-m", "debugpy.adapter" },
+					options = {
+						source_filetype = "python",
+					},
+				})
+			end
+		end
+		dap.configurations.python = {
+			{
+				type = 'python';
+				request = 'launch';
+				name = "Launch file";
+
+
+				program = "${file}";
+				pythonPath = function()
+					local cwd = vim.fn.getcwd()
+					if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+						return cwd .. '/venv/bin/python'
+					elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+						return cwd .. '/.venv/bin/python'
+					else
+						return '/usr/bin/python'
+					end
+				end;
+			},
+		}
+
+
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
