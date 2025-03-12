6ca22a33fc30b8a6f2c916bb9ed10739976e0e2b
QTimos <timos.wapo@protonmail.com>
2025-03-12 23:07:55 +0000

    some modifications to vimOpts.lua and remap.lua

--- lua/Timos/remap.lua
+++ lua/Timos/remap.lua
@@ -1,5 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>")
+vim.keymap.set("n", "<A-o>", ":only<CR>")
+
 vim.keymap.set("n", "<leader>la", ":Lazy<CR>")
 vim.keymap.set("n", "<leader>ma", ":Mason<CR>")
-vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
--- lua/Timos/vimOpts.lua
+++ lua/Timos/vimOpts.lua
@@ -1,23 +1,28 @@
 vim.g.python3_host_prog = "~/.venv/bin/python"
 
--- vim.api.nvim_create_autocmd("VimEnter", {
---     pattern = "*",
---     callback = function()
---         if vim.fn.argc() == 0 then
---             -- If in a directory, open netrw (Explore)
---             -- vim.cmd("Explore")
---             -- Close neotree if it's automatically opened
---             -- vim.cmd("Neotree close")
---         end
---     end,
--- })
-
-
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set colorcolumn=200")
+vim.cmd("set updatetime=40")
+vim.o.wrap = false
+
+vim.o.swapfile = false
+vim.o.backup = false
+
 vim.cmd("set autoindent")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
+
+vim.cmd("set scrolloff=10")
+vim.o.signcolumn = "yes"
+
 vim.cmd("set smarttab")
 vim.cmd("set mouse=a")
+
+vim.cmd("set termguicolors")
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+
